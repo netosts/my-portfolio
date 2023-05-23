0f755bea1630592c9d17050939d48e8434433a9c
snufera <netostt91@gmail.com>
2023-05-22 21:08:30 -0300

    0.2.1 - Profile section done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,26 @@
 </script>
 
 <template>
-  <h1>Olá</h1>
+  <main>
+    <section class="profile">
+      <div class="profilebox">
+        <div class="profile__greetings">
+          <h1 class="profile--large">Olá!</h1>
+          <h1 class="profile--medium">Eu sou Neto!</h1>
+        </div>
+        <div class="profile__pic">
+          <img src="../assets/images/profile-pic.png" alt="">
+        </div>
+        <div class="profile__info">
+          <h2>{Coder}</h2>
+          <p>Atuando como desenvolvedor Front end</p>
+        </div>
+      </div>
+    </section>
+    <section class="projects"></section>
+  </main>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import '../assets/scss/homeview.scss';
+</style>
